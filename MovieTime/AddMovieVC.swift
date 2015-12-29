f18a1ec596298f0f86f279cac114c700a961b332
shaynemeyer <shayne.meyer@gmail.com>
2015-12-29 08:23:09 -0800

    finished add movie controller

--- MovieTime/AddMovieVC.swift
+++ MovieTime/AddMovieVC.swift
@@ -7,27 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
-class AddMovieVC: UIViewController {
+class AddMovieVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var movieTitle: UITextField!
     @IBOutlet weak var movieDesc: UITextField!
     @IBOutlet weak var movieURL: UITextField!
     @IBOutlet weak var moviePlot: UITextField!
+    @IBOutlet weak var movieImg: UIImageView!
     
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        movieImg.layer.cornerRadius = 4.0
+        movieImg.clipsToBounds = true
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        movieImg.image = image
     }
 
     @IBAction func addMovieBtnPressed(sender: AnyObject) {
-        
+        if let title = movieTitle.text where title != "" {
+            let app = UIApplication.sharedApplication().delegate as! AppDelegate
+            let context = app.managedObjectContext
+            let entity = NSEntityDescription.entityForName("Movie", inManagedObjectContext: context)!
+            
+            let movie = Movie(entity: entity, insertIntoManagedObjectContext: context)
+            movie.title = movieTitle.text
+            movie.desc = movieDesc.text
+            movie.imdb = movieURL.text
+            movie.plot = moviePlot.text
+            movie.setMovieImage(movieImg.image!)
+            
+            context.insertObject(movie)
+            
+            do {
+                try context.save()
+            } catch {
+                print("Could not save movie")
+            }
+            
+            self.navigationController?.popViewControllerAnimated(true)
+        }
     }
     
     @IBAction func addCoverBtnPressed(sender: AnyObject) {
-        
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
 }
--- MovieTime/MovieDetailVC.swift
+++ MovieTime/MovieDetailVC.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class MovieDetailVC: UIViewController {
 
+    @IBOutlet weak var movieImg: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDesc: UILabel!
+    @IBOutlet weak var movieURL: UILabel!
+    @IBOutlet weak var moviePlot: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
 }
